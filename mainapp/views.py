cd381f8ffde711d33610f47475629cc046e9c501
marina <inwaiting2010@gmail.com>
2018-07-12 16:59:17 +0300

    fix

--- mainapp/views.py
+++ mainapp/views.py
@@ -21,3 +21,26 @@ def amclassic(request):
 
 def rusclassic(request):
     return render(request, 'mainapp/rusclassic.html')
+
+
+def capitan(request):
+    return render(request, 'mainapp/capitan.html')
+
+
+def kill(request):
+    return render(request, 'mainapp/kill.html')
+
+
+def mice(request):
+    return render(request, 'mainapp/mice.html')
+
+def nadprop(request):
+    return render(request, 'mainapp/nadprop.html')
+
+
+def prest_nakaz(request):
+    return render(request, 'mainapp/prest_nakaz.html')
+
+
+def voina_i_mir(request):
+    return render(request, 'mainapp/voina_i_mir.html')
