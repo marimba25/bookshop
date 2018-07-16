a2ce10bd3edc111a5959f944f254adfb475271f1
marina <inwaiting2010@gmail.com>
2018-07-16 09:46:59 +0300

    fix admin

--- mainapp/admin.py
+++ mainapp/admin.py
@@ -2,9 +2,10 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Book, Author, Country
+from .models import Book, Author, Country, BookCategory
 admin.site.register(Book)
 admin.site.register(Author)
 admin.site.register(Country)
+admin.site.register(BookCategory)
 
 
