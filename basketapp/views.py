8d22b60c751cc8a22254493015e9cf883bde6cfd
marina <inwaiting2010@gmail.com>
2018-08-15 10:14:02 +0300

    login required

--- basketapp/views.py
+++ basketapp/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, HttpResponseRedirect, get_object_or_404
 from basketapp.models import Basket
 from mainapp.models import Book
+from django.contrib.auth.decorators import login_required
 
 
+@login_required
 def basket(request):
     basket_items = Basket.objects.filter(user=request.user).order_by('product__category')
     content = {
@@ -11,6 +13,7 @@ def basket(request):
     return render(request, 'basketapp/basket.html', content)
 
 
+@login_required
 def basket_add(request, pk):
     product = get_object_or_404(Book, pk=pk)
     old_basket_item = Basket.objects.filter(user=request.user, product=product)
@@ -26,6 +29,7 @@ def basket_add(request, pk):
     return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
 
 
+@login_required
 def basket_remove(request, pk):
     basket_record = get_object_or_404(Basket, pk=pk)
     basket_record.delete()
