534013822061e25d9d259bf63c8f1591bc2d3f8b
marina <inwaiting2010@gmail.com>
2018-11-17 22:41:57 +0300

    fix

--- ordersapp/urls.py
+++ ordersapp/urls.py
@@ -17,6 +17,7 @@ urlpatterns = [
             name='order_read'),
     re_path(r'^forming/complete/(?P<pk>\d+)/$',
             ordersapp.order_forming_complete, name='order_forming_complete'),
+    re_path(r'^product/(?P<pk>\d+)/price/$', ordersapp.get_product_price),
 
 ]
 
